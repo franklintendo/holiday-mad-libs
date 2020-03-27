3a8a998fcd42f06c621c77edca1814810240f4df
Frank Moran <info@francisfrankmoran.com>
2020-03-26 21:22:57 -0400

    github pages

--- src/components/MadLibChoices.js
+++ src/components/MadLibChoices.js
@@ -9,15 +9,15 @@ function MadLibForm({ handleStoryChoice }) {
           <h2 className="text-center" style={{width: "auto"}}>STEP 2: Choose your story</h2>
           <div className="d-inline-block">
 
-            <img onClick={handleStoryChoice} className='holiday-btn mx-2' src={require('../img/icons/halloweenicon.jpg')} alt="Halloween" name="halloween" width="58" height="58" />
+            <img onClick={handleStoryChoice} className='holiday-btn mx-2 my-3' src={require('../img/icons/halloweenicon.jpg')} alt="Halloween" name="halloween" width="58" height="58" />
 
-            <img onClick={handleStoryChoice} className='holiday-btn mx-2' src={require('../img/icons/fourthjulyicon.jpg')} alt="Fourth of July" name="fourthjuly" width="58" height="58" />
+            <img onClick={handleStoryChoice} className='holiday-btn mx-2 my-3' src={require('../img/icons/fourthjulyicon.jpg')} alt="Fourth of July" name="fourthjuly" width="58" height="58" />
 
-            <img onClick={handleStoryChoice} className='holiday-btn mx-2' src={require('../img/icons/xmasicon.jpg')} alt="Christmas" name="xmas" width="58" height="58" />
+            <img onClick={handleStoryChoice} className='holiday-btn mx-2 my-3' src={require('../img/icons/xmasicon.jpg')} alt="Christmas" name="xmas" width="58" height="58" />
 
-            <img onClick={handleStoryChoice} className='holiday-btn mx-2' src={require('../img/icons/thanksgivingicon.jpg')} alt="Thanksgiving" name="thanksgiving" width="58" height="58" />
+            <img onClick={handleStoryChoice} className='holiday-btn mx-2 my-3' src={require('../img/icons/thanksgivingicon.jpg')} alt="Thanksgiving" name="thanksgiving" width="58" height="58" />
 
-            <img onClick={handleStoryChoice} className='holiday-btn mx-2' src={require('../img/icons/nyeveicon.jpg')} alt="New Year's Eve" name="nye" width="58" height="58" />
+            <img onClick={handleStoryChoice} className='holiday-btn mx-2 my-3' src={require('../img/icons/nyeveicon.jpg')} alt="New Year's Eve" name="nye" width="58" height="58" />
           </div>
       </div>
   );
